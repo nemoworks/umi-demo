5964076431d8d1ffdeb620e65eb41a0d126886d2
Benjamin15122 <15051183877@163.com>
2021-04-06 01:04:06 +0800

    Route example

---
+++ src/layouts/index.js
@@ -0,0 +1,25 @@
+import { Layout, Menu} from 'antd';
+import { Link } from 'umi';
+import { AppstoreOutlined, UserOutlined, TeamOutlined } from '@ant-design/icons';
+
+const { Header, Content, Footer } = Layout;
+function BasicLayout(props) {
+  return (
+  <Layout>
+    <Header>
+      <div/>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1"><Link to="/"><AppstoreOutlined/>index</Link></Menu.Item>
+        <Menu.Item key="2"><Link to="/employee"><UserOutlined/>employee</Link></Menu.Item>
+        <Menu.Item key="3"><Link to="/list"><TeamOutlined/>list</Link></Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      {props.children}
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
+  );
+}
+
+export default BasicLayout;
